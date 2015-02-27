4bebd7bc2fa2b074ae30444a7ba9d14723b67cd7
ivana korovesovska <ivana.korovesovska@gmail.com>
2015-02-27 10:26:58 +0100

    27 fev

---
+++ c8.php
@@ -0,0 +1,50 @@
+<?php 
+
+	function zdravo(){
+		echo "Zdravo Svetu";
+		echo "<br>";
+	}
+
+	zdravo();
+	zdravo();
+	zdravo();
+	zdravo();
+	zdravo();
+	zdravo();
+	zdravo();
+	zdravo();
+
+	function pero(){
+		echo "Pero nash golem maz";
+		echo "<br>";
+	}
+
+	pero();
+	pero();
+	pero();
+	pero();
+	pero();
+	pero();
+	pero();
+	pero();
+	pero();
+	pero();
+	pero();
+	pero();
+
+	function mleko($tip){
+		echo $tip;
+	}
+
+	mleko('Bitolsko');
+	mleko('Tursko');
+
+	function sobiranje($a, $b){
+		echo "<hr>";
+		echo $a + $b;
+	}
+
+	sobiranje(4, 6);
+	sobiranje(10, 57);
+	sobiranje(254640, 5214234357);
+ ?>
