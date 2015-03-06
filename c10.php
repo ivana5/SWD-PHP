ce6a3bef2785da108c5cafe8c534a289e0303b31
ivana korovesovska <ivana.korovesovska@gmail.com>
2015-03-06 12:44:35 +0100

    6 mart

---
+++ c10.php
@@ -0,0 +1,64 @@
+<?php 
+
+// for (/*start*/; /*kraj*/; /*chekor-step*/){
+	/*kod*/
+//}
+
+// $i = $i + 1;
+// $i +=1;
+// $i++;
+
+// for ($i = 0; $i < 10; $i++){ 
+// 	echo $i;
+// 	echo'<br>';
+// }
+
+// for ($i = 10; $i < 100 ; $i += 2) { 
+// 	echo $i;
+// 	echo '<br>';
+// }
+
+// echo '<hr>';
+
+// for ($i = -8; $i < 16 ; $i += 3) { 
+// 	echo $i;
+// 	echo '<br>';
+// }
+
+// echo '<hr>';
+
+// for ($i = 20; $i < 22; $i++) { 
+// 	echo $i;
+// 	echo '<br>';
+// }
+
+// echo '<hr>';
+
+// for ($i = 0; $i < 111; $i += 8) { 
+// 	echo $i;
+// 	echo '<br>';
+// }
+
+// echo '<hr>';
+
+// for ($i = 0; $i > -11; $i -= 2) { 
+// 	echo $i;
+// 	echo '<br>';
+// }
+
+// for ($i = 0; $i < 30 ; $i++) { 
+// 	if ($i % 2 == 0) {
+// 		echo $i;
+// 		echo '<br>';
+// 	}
+// }
+
+$broj = 0;
+
+for ($i = 10; $i < 50; $i++) { 
+	 $broj += $i;
+}
+
+echo $broj;
+
+ ?>
---
+++ c9.php
@@ -0,0 +1,86 @@
+<?php 
+
+//$a = 1;
+//echo $a;
+
+//$a = array(3, 5, 1, 8, 11);
+//echo $a;
+
+//print_r($a);
+
+//echo $a[0];
+
+//$a = array();
+
+//$a[] = 'pero';
+//$a[] = 'janko';
+//$a[] = 'stanko';
+
+//print_r($a);
+
+// $a[5] = 'Skopje';
+// $a[11] = 'Tetovo';
+// $a[101] = 'Gostivar';
+// $a[6] = 'Gevgelija';
+// $a[] = 'Kumanovo';
+
+// print_r($a);
+
+// $a = array(10 => 'Makedonija', 20 => 'Germanija', 50 => 'Shvicarska');
+// print_r($a);
+
+// $a = array();
+
+// $a[] = 'jajca';
+// $a[] = 'pica';
+// $a[] = 'salata';
+// $a[] = 'stek';
+// $a[] = 'shpageti';
+// $a[] = 'kornfleks';
+// $a[] = 'kebapi';
+// $a[] = 'pileshko';
+// $a[] = 'burek';
+// $a[] = 'kavijar';
+
+// print_r($a);
+
+// $a = array(
+// 	'string', 
+// 	TRUE, 
+// 	1001, 
+// 	3.14,
+// 	array(
+// 		'pero',
+// 		'janko',
+// 		'stanko',
+// 	),
+//  	array(
+//  		'new york',
+//  		'paris',
+//  		'london'
+//  	)
+// );
+
+// echo $a[4][1];
+// echo '<br>';
+// echo $a[5][0];
+// echo '<br>';
+// echo $a[2];
+// echo '<br>';
+// echo $a[3];
+// echo '<br>';
+// echo $a[4][2];
+
+
+$student = array(
+	array (0 => 'Bojan', 1 => 'Gavrovski', 2 => 223305), 
+	array (0 => 'Pero', 1 => 'Perovski', 2 => 455025 ),
+	array (0 => 'Janko', 1 => 'Jankovski', 2 => 256698),
+	array (0 => 'Stanko', 1 => 'Stankovski', 2 => 366978));
+
+
+print_r($student);
+
+
+
+ ?>
