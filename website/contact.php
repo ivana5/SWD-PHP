d757b3aaf19d5a999fb416424be28fa6952b0620
ivana5 <ivana.korovesovska@gmail.com>
2015-04-24 07:48:48 +0200

    Revert "24ti april"
    This reverts commit 2ccfe8b0c635e2f2107adfa4351998233d181a09.

---
+++ website/contact.php
@@ -0,0 +1,11 @@
+<h2>Contact</h2>
+<p>
+	Contact form
+</p>
+<form action="">
+	<input type="text">
+	<br>
+	<textarea></textarea>
+	<br>
+	<button>Send</button>
+</form>
---
+++ website/gallery.php
@@ -0,0 +1,5 @@
+<h2>Gallery</h2>
+<p>
+	These are the images of the gallery
+</p>
+<img src="../uploads/Desert.jpg" alt="">
